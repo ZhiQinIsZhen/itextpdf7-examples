e27aa8d784e38e88e1d4333c107736bff043c593
liyangzhen <liyangzhen@qjdchina.com>
2022-09-29 17:22:40 +0800

    add demo3

--- src/main/java/com/lyz/service/pdf/controller/Demo4Controller.java
+++ src/main/java/com/lyz/service/pdf/controller/Demo4Controller.java
@@ -1,14 +1,8 @@
 package com.lyz.service.pdf.controller;
 
-import com.itextpdf.kernel.events.PdfDocumentEvent;
-import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
-import com.lyz.service.pdf.handler.WaterMarkEventHandler;
 import com.lyz.service.pdf.result.Result;
-import com.lyz.service.pdf.util.PdfFontUtil;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -28,7 +22,7 @@ import java.io.IOException;
  * @version 1.0.0
  * @date 2022/9/29 11:22
  */
-@Api(tags = "demo3")
+@Api(tags = "demo4")
 @ApiResponses(value = {
         @ApiResponse(code = 0, message = "成功"),
         @ApiResponse(code = 1, message = "失败")
@@ -36,24 +30,15 @@ import java.io.IOException;
 @Slf4j
 @Validated
 @RestController
-@RequestMapping("/pdf/demo3")
-public class Demo3Controller {
+@RequestMapping("/pdf/demo4")
+public class Demo4Controller {
 
-    @ApiOperation("生成PDF文件--增加水印功能")
+    @ApiOperation("生成PDF文件")
     @PostMapping("/create")
     public Result<Boolean> create(@ApiParam(name = "filename")
             @Valid @NotBlank(message = "文件生成地址不能为空")
             @RequestParam(value = "filename", defaultValue = "") String filename) throws IOException {
         PdfDocument pdf = new PdfDocument(new PdfWriter(filename));
-        pdf.addEventHandler(PdfDocumentEvent.START_PAGE, new WaterMarkEventHandler("杭州至秦科技有限公司"));
-        Document document = new Document(pdf);
-        //add font
-        PdfFont font = PdfFontUtil.createFont(PdfFontUtil.FontEnum.WRYH);
-        document.add(new Paragraph("Hello World!").setFont(font).setFontSize(20f));
-        pdf.addNewPage();
-        pdf.addNewPage();
-        pdf.addNewPage();
-        document.close();
         return Result.success(Boolean.TRUE);
     }
 }
